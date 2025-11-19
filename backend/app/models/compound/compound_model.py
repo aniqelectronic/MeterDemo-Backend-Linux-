5e7115f5adac375eb0e8355a1894b2a46280fbcc
ANIQLT\User <aniq_electronic@raisevest.com.m>
2025-11-19 16:18:48 +0800

    Revert "Fix error compound"
    This reverts commit bb272c76f3bb8918a1fcd6b6fbe95b3f2805f676.

--- backend/app/models/compound/compound_model.py
+++ backend/app/models/compound/compound_model.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from datetime import date, time
 from enum import Enum
-from typing import Optional
+
 
 class StatusTypeEnum(str, Enum):
     paid = "PAID"
@@ -15,7 +15,7 @@ class CompoundBase(BaseModel):
     time: time
     offense: str
     amount: float
-    status: Optional[StatusTypeEnum] = None
+    status: StatusTypeEnum
 
 class CompoundCreate(CompoundBase):
     pass
