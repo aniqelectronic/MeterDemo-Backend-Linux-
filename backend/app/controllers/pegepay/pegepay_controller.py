6719549b86f863cbc96711b091e22b48aec810f7
ANIQLT\User <aniq_electronic@raisevest.com.m>
2026-02-23 15:56:02 +0800

    pegepay qr v2

--- backend/app/controllers/pegepay/pegepay_controller.py
+++ backend/app/controllers/pegepay/pegepay_controller.py
@@ -313,7 +313,7 @@ def iframe_wrapper(iframe_url: str):
              height: 1400px;  /* keep original height */
              border: none;
          
-    transform: scale(2.0) translateX(-70%);
+    transform: scale(2.0) translateX(-50%);
     transform-origin: top left;
 
             }}
