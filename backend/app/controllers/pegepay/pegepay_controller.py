708bfa025bb731942fa04d71e0abd06c6f703cc7
ANIQLT\User <aniq_electronic@raisevest.com.m>
2026-05-04 14:33:37 +0800

    fix qr loading v1

--- backend/app/controllers/pegepay/pegepay_controller.py
+++ backend/app/controllers/pegepay/pegepay_controller.py
@@ -482,7 +482,7 @@ def iframe_wrapper(iframe_url: str):
 
             .iframe-container {{
                 width: 100vw;
-                height: 100vh;
+                height: 65vh;
                 overflow: hidden;
                 display: flex;
                 justify-content: center;
